65fe23f3be2f1e5c8fb346693abb9d8c93af2697
Pierre <pierre.pocheron@gmail.com>
2021-07-02 11:25:41 +0200

    init ex 4

---
+++ src/app/app-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-app-input',
+  template: `
+    <input type="text" id="inputVal" value="{{inputOld}}" (input)="inputNew.emit(inputVal.value)" #inputVal/>
+    <p>Message : {{inputOld}}</p>
+  `,
+  styles: [
+  ]
+})
+export class AppInputComponent {
+  @Input() inputOld: string = ""
+  @Output() inputNew = new EventEmitter<string>();
+}
+
+aaa
+
+aazezae
