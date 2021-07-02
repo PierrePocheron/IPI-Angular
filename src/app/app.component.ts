7fd692d95d8d9b172531e08145c07451cacc2024
Pierre <pierre.pocheron@gmail.com>
2021-07-02 11:24:50 +0200

    init exercice 1-2-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>Exercice 1 - Hello world</h1>
+  <h2>Exercice 2 - 3</h2>
+  <app-app-input [inputOld]="inputValue" (inputNew)="inputValue = $event"></app-app-input>
+  <app-app-input [inputOld]="inputValue" (inputNew)="inputValue = $event"></app-app-input>
+  `,
+  styles: []
 })
 export class AppComponent {
-  title = 'demo';
+  inputValue: string = "";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppInputComponent } from './app-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
